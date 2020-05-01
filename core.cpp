bca9db6a8161659c9486cac6f50eb821ab784c60
ddwbb <sintsquad@gmail.com>
2020-05-01 08:17:39 +0300

    added few methods

--- core.cpp
+++ core.cpp
@@ -2,10 +2,45 @@
 
 Core::Core(QObject *parent) : QObject(parent)
 {
-    window = new Window;
+    m_window = new Window;
+
+}
+
+Core::~Core()
+{
+    delete m_window;
+    if (m_buffer)
+        delete m_buffer;
+    if (m_bufferArray)
+        delete m_buffer;
 }
 
 void Core::operator()()
 {
-    window->show();
+    m_window->show();
+}
+
+void Core::init()
+{
+
+}
+
+void Core::changeTime()
+{
+
+}
+
+void Core::startReceive()
+{
+
+}
+
+void Core::stopReceiveByTimer()
+{
+
+}
+
+void Core::stopReceiveByInterrupt()
+{
+
 }
