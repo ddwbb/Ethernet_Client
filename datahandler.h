ff3edf6ef8459887abcf236efbcd18fc8c422d29
ddwbb <sintsquad@gmail.com>
2020-05-08 13:38:32 +0300

    Added DataHandler class

---
+++ datahandler.h
@@ -0,0 +1,36 @@
+#ifndef DATAHANDLER_H
+#define DATAHANDLER_H
+
+#include <QFile>
+#include <QThread>
+#include <QObject>
+#include <QTextStream>
+
+#include "config.h"
+
+class DataHandler : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DataHandler(DataHandlerParams, QObject *parent = nullptr);
+
+public slots:
+    void write();
+    void prepareFile();
+
+signals:
+    void dataWritten();
+
+private:
+    QFile m_file;
+    DataHandlerParams m_params;
+
+    bool m_overrided;
+    bool m_receiveTerminated;
+    unsigned short m_receiveIndex;
+    unsigned short m_writtenIndex;
+
+    void getParams();
+};
+
+#endif // DATAHANDLER_H
